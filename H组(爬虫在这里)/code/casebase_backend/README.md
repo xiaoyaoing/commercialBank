309e591ac3373e2964c291fe763cbd064de92d90
Draco <191250037@smail.nju.edu.cn>
2022-10-05 13:02:53 +0800

    config mongo

--- H组(爬虫在这里)/code/casebase_backend/README.md
+++ H组(爬虫在这里)/code/casebase_backend/README.md
@@ -1,3 +1,13 @@
 # caseBase_backEnd
 
 银保监会案例库后端
+
+## 配置
+
+mysql
+
+mongoDB
+
+修改src/main/java/com/example/caseBase/util/ExtractInformation.java里csv文件路径。
+
+elasticsearch ?
--- H组(爬虫在这里)/code/casebase_backend/src/test/java/com/example/caseBase/util/clawerTest.java
+++ H组(爬虫在这里)/code/casebase_backend/src/test/java/com/example/caseBase/util/clawerTest.java
@@ -8,18 +8,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class clawerTest {
     @Autowired
     Clawer clawer;
     @Test
     public void clawAllTest() throws IOException, ParseException, InterruptedException {
-        clawer = new Clawer();
+//        clawer = new Clawer();
 
         clawer.getDocURL(1);
 //        clawer.getDocURL(2);
@@ -27,7 +26,7 @@ public class clawerTest {
     }
     @Test
     public void clawOneTest() throws IOException {
-        clawer = new Clawer();
+//        clawer = new Clawer();
         clawer.getHtml(1003000L,"1",new Date());
     }
 }
